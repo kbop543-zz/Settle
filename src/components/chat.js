d476099e395dc3356a093ebe1eb50178ffb282ea
Misan Iwere <temisan.iwere@mail.utoronto.ca>
2018-02-03 04:06:10 -0500

    chat page

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,2 +1,10 @@
 import React, { Component } from 'react';
 import {Text,View} from 'react-native';
+
+export default class Chat extends Component{
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
