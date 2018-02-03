e21d2579c4bfba5a2f45350f8b06b3351676748d
Misan Iwere <temisan.iwere@mail.utoronto.ca>
2018-02-03 05:13:52 -0500

    added backend for chatbot

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 import {Text,View} from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
+
+import ChatController from '../controllers/chatcontroller.js';
 
 export default class Chat extends Component{
+  state = {
+    messages: []
+  };
+
+  componentWillMount(){
+
+  }
   render() {
     return (
-      <Text>Hello world!</Text>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(message) => {
+          ChatController.sendMessage(message);
+        }}
+        user={{
+          _id: ChatController.getUid(),
+          name: this.props.name,
+        }}
+      />
     );
   }
+
+  componentDidMount() {
+    ChatController.loadMessages((message) => {
+      this.setState((previousState) => {
+        return {
+          messages: GiftedChat.append(previousState.messages, message),
+        };
+      });
+    });
+  }
+  componentWillUnmount() {
+    ChatController.closeChat();
+  }
+}
+
+Chat.defaultProps = {
+  name: 'Cool Person!',
 }
